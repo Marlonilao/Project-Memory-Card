aed36f4448a5a04fabf6b57025d42ffda5f5e62f
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-05-09 16:53:24 +0800

    feat: fetch Pokemon list with sprites

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
 import '@mantine/core/styles.css'
 import { MantineProvider, Container, Notification, Text } from '@mantine/core'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from './components/Header'
 
 function App() {
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+  const [clickedCards, setClickedCards] = useState([])
+  const [cards, setCards] = useState([])
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await axios.get(
+        'https://pokeapi.co/api/v2/pokemon/?limit=12/',
+      )
+
+      const pokemonsWithSprites = await Promise.all(
+        response.data.results.map(async (pokemon) => {
+          const pokemonData = await axios.get(pokemon.url)
+          return {
+            ...pokemon,
+            sprite: pokemonData.data.sprites.front_default.toString(),
+          }
+        }),
+      )
+
+      setPokemons(pokemonsWithSprites)
+      console.log(pokemonsWithSprites)
+    }
+    fetchPokemons()
+  }, [])
+
   return (
     <MantineProvider>
       <Container>
