5b209366bf188bdc78b2e3926d2512bd2bb93eb8
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-05-09 14:31:55 +0800

    add Notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import '@mantine/core/styles.css'
-import { MantineProvider, Container } from '@mantine/core'
+import { MantineProvider, Container, Notification, Text } from '@mantine/core'
 import Header from './components/Header'
 
 function App() {
@@ -7,6 +7,19 @@ function App() {
     <MantineProvider>
       <Container>
         <Header score={0} bestScore={0} />
+        <Notification
+          withCloseButton={false}
+          withBorder
+          color='grape'
+          mt='md'
+          bg='grape.0'
+          radius='md'
+        >
+          <Text size='xs' c='grape'>
+            Click every card once — the cards shuffle after each click. Clicking
+            the same card twice resets your score
+          </Text>
+        </Notification>
       </Container>
     </MantineProvider>
   )
