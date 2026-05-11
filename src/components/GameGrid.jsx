f7e376d2a67b828614553f2c600bdcac673dcf14
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-05-11 12:40:07 +0800

    fix: set fixed colspan of 2 to prevent sprite misalignment in cards

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,23 +1,7 @@
 import { Grid, Image, Card, UnstyledButton } from '@mantine/core'
 import classes from './GameGrid.module.css'
 
-let colspan
-
 const GameGrid = (props) => {
-  switch (props.pokemons.length) {
-    case 12:
-      colspan = 3
-      break
-    case 18:
-      colspan = 2
-      break
-    case 24:
-      colspan = 2
-      break
-    default:
-      colspan = 3
-  }
-
   return props.pokemons ? (
     <Grid gap='xs' mt='xl'>
       {props.pokemons.map((pokemon) => {
@@ -25,7 +9,7 @@ const GameGrid = (props) => {
         const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
 
         return (
-          <Grid.Col span={colspan} key={pokemon.name}>
+          <Grid.Col span={2} key={pokemon.name}>
             <Card withBorder radius='md' className={classes.card}>
               <UnstyledButton
                 w='100%'
